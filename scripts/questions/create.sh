307a2efe96b1efb22e5da39396dedee6f1633f73
jal90 <mezzyjezze@gmail.com>
2018-03-26 11:27:45 -0400

    Add questions CURL scripts folder, edit surveys CURL scripts
    Fixed typos in surveys scripts from 'examples' to 'surveys'

--- scripts/questions/create.sh
+++ scripts/questions/create.sh
@@ -3,9 +3,9 @@
 API="http://localhost:4741"
 URL_PATH="/surveys"
 
-curl "${API}${URL_PATH}/${ID}" \
+curl "${API}${URL_PATH}" \
   --include \
-  --request PATCH \
+  --request POST \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
   --data '{
